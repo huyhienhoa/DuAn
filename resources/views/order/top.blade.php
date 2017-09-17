0a8573af9e12902ecd608134ffb63754fd69592f
Huy Hien Hoa <huyhienhoacnt@gmail.com>
2017-09-17 16:57:26 +0700

    Quản lý hóa đơn

---
+++ resources/views/order/top.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 17/09/2017
+ * Time: 8:54 AM
+ */
---
+++ resources/views/order/view.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 17/09/2017
+ * Time: 9:57 AM
+ */
