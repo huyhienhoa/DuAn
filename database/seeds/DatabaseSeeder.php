e89e8c93f6acd081cc2f7e355ea6c085afae048f
Huy Hien Hoa <huyhienhoacnt@gmail.com>
2017-09-21 16:15:48 +0700

    fix seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,15 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
+
          $this->call(UsersTableSeeder::class);
-         $this->call(ProductsTableSeeder::class);
-=======
-        $this->call(UsersTableSeeder::class);
         $this->call(BranchesTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
+        $this->call(ProductsTableSeeder::class);
 
-
->>>>>>> f69ec5727e9be290e344eeffd46e38c9c3b1f05a
     }
 }
