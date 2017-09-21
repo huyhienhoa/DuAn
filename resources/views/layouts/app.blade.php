d16888444ff3e2c3185b6f4fd31b0366eb5d73a1
kite2 <dotienha@gmail.com>
2017-09-21 09:30:17 +0700

    them co so du lieu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Trang Chủ</title>
+    <title>Trang quan ly</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
